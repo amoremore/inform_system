c11096a528906224445b420cb3f760a267187dc0
amoremore <51174611+amoremore@users.noreply.github.com>
2020-06-03 20:59:48 +0300

    03.06.20/second commit

--- Inf_sys(geogr)/Program.cs
+++ Inf_sys(geogr)/Program.cs
@@ -16,7 +16,7 @@ namespace Inf_sys_geogr_
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main_user());
         }
     }
 }
